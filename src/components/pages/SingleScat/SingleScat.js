3cd2cffdc677de7224c764b77d4a708dabfc45d1
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-06-08 00:06:52 -0500

    added proptypes

--- src/components/pages/SingleScat/SingleScat.js
+++ src/components/pages/SingleScat/SingleScat.js
@@ -4,31 +4,32 @@ import './SingleScat.scss';
 import scatsData from '../../../helpers/data/scatsData';
 
 class SingleScat extends React.Component {
- state = {
-   scat:{},
- }
+  state = {
+    scat: {},
+  }
 
- componentWillMount() {
-   const { scatId } = this.props.match.params;
-   scatsData.gitSingleScat(scatId)
-   .then((response) => this.setState({ scat: response.data }))
-   .catch((err) => console.error('unable to get single scat: ',err));
- }
- render() {
-   const { scat } = this.state;
-   return (
-    <div className="SingleScat" style={{ backgroundColor: scat.color }}>
-    <h1>{scat.location}</h1>
-    <p>Shape: {scat.shape}</p>
-    <p>Size: {scat.size}</p>
-    <p>Temperature: {scat.temperature}</p>
-    <p>Viscosity: {scat.viscosity}</p>
-    <p>Location: {scat.location}</p>
-    <p>Notes: {scat.notes}</p>
-    <p>Was it Fulfilling? {scat.wasFulfilling ? 'Yes' : 'No'}</p>
-  </div>
-   );
- }
+  componentDidMount() {
+    const { scatId } = this.props.match.params;
+    scatsData.getSingleScat(scatId)
+      .then((response) => this.setState({ scat: response.data }))
+      .catch((err) => console.error('unable to get single scat: ', err));
+  }
+
+  render() {
+    const { scat } = this.state;
+    return (
+      <div className="SingleScat" style={{ backgroundColor: scat.color }}>
+        <h1>{scat.location}</h1>
+        <p>Shape: {scat.shape}</p>
+        <p>Size: {scat.size}</p>
+        <p>Temperature: {scat.temperature}</p>
+        <p>Viscosity: {scat.viscosity}</p>
+        <p>Location: {scat.location}</p>
+        <p>Notes: {scat.notes}</p>
+        <p>Was it Fulfilling? {scat.wasFulfilling ? 'Yes' : 'No'}</p>
+      </div>
+    );
+  }
 }
 
 export default SingleScat;
